3af928252a99c345b6c8fe8f5d163eea4229ff83
Laszlo Bozzay <laszlo_bozzay@epam.com>
2022-06-24 16:22:49 +0200

    feat: added new stuff

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 function multipliesByTwo(num) {
   return num * 2
 }
+function helloWorld() {
+  return "hello world"
+}
+
 
 function showMsg() {
+  //hi
+  console.log(helloWorld());
   return 'Hello There......'
 }
 
-document.body.addEventListener("click", ()=> alert(showMsg()))
+document.body.addEventListener("click", function () {
+  alert(helloWorld())
+})
